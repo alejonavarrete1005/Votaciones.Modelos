77b048cb9a1667bbaae8555dfc998a29e1094fd2
alejo10005 <alejonavarrete05@icloud.com>
2025-12-28 14:41:28 -0500

    creado carpeta enums

---
+++ Votaciones.Modelos/Enums/RolEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Votaciones.Modelos.Enums
+{
+    public enum RolEnum
+    {
+        Administrador = 1,
+        Votante = 2,
+        Candidato = 3
+    }
+
+}
---
+++ Votaciones.Modelos/Enums/TipoEleccionEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Votaciones.Modelos.Enums
+{
+    public enum TipoEleccionEnum
+    {
+        Nominal = 1,
+        Plancha = 2
+    }
+
+}
